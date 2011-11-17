89ef63b8abe79f8bc335d3d4db1d7fd0179126ce
Jacob Oscarson <jacob@plexical.com>
2011-11-17 12:09:00 +0100

    Test with an object handler

--- responder/test/test_responder.py
+++ responder/test/test_responder.py
@@ -1,4 +1,9 @@
 from responder import Responder
 
+from responder.test.support import Expando
+
 def test_init_w_str():
     assert Responder('labmod')
+
+def test_init_w_obj():
+    assert Responder(Expando())
